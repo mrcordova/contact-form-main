26246ca5184d563bb212ea6bb4eaf57106c79ab6
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-06-05 11:08:53 -0700

    logic for error checking

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
 const submitBtn = document.querySelector('.submit-btn');
+const inputs = document.querySelectorAll('input[type=text], input[type=email], textarea');
+const checkInput = document.querySelector('input[type=checkbox]');
+const radioInputs = document.querySelectorAll('input[name=query]');
+const dialog = document.querySelector('dialog');
 
 
 
 
 submitBtn.addEventListener('click', (e) => {
     e.preventDefault();
+    const spanCheck = document.querySelector('#checkbox-error');
+    const spanRadio = document.querySelector('#radio-error');
+    for (const input of inputs) {
+        input.classList.toggle('form-error-border', !input.checkValidity());
+        input.nextElementSibling.classList.toggle('form-error-hide', !input.classList.contains('form-error-border'));
+    }
+    for (const input of radioInputs) {
+        spanRadio.classList.toggle('form-error-hide', input.checkValidity());
+    }
+    spanCheck.classList.toggle('form-error-hide', checkInput.checkValidity());
+    
+    
 } )
