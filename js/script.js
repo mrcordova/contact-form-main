4e9c6d2479c29be6477b5a45c1f3801157619edd
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-06-05 18:14:06 -0700

    formatting code

--- js/script.js
+++ js/script.js
@@ -1,26 +1,30 @@
-const submitBtn = document.querySelector('.submit-btn');
-const inputs = document.querySelectorAll('input[type=text], input[type=email], textarea');
-const checkInput = document.querySelector('input[type=checkbox]');
-const radioInputs = document.querySelectorAll('input[name=query]');
-const dialog = document.querySelector('dialog');
-const form = document.getElementById('contact-form');
+const submitBtn = document.querySelector(".submit-btn");
+const inputs = document.querySelectorAll(
+  "input[type=text], input[type=email], textarea"
+);
+const checkInput = document.querySelector("input[type=checkbox]");
+const radioInputs = document.querySelectorAll("input[name=query]");
+const dialog = document.querySelector("dialog");
+const form = document.getElementById("contact-form");
 
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const spanCheck = document.querySelector("#checkbox-error");
+  const spanRadio = document.querySelector("#radio-error");
+  for (const input of inputs) {
+    input.classList.toggle("form-error-border", !input.checkValidity());
+    input.nextElementSibling.classList.toggle(
+      "form-error-hide",
+      !input.classList.contains("form-error-border")
+    );
+  }
+  for (const radioInput of radioInputs) {
+    spanRadio.classList.toggle("form-error-hide", radioInput.checkValidity());
+  }
 
+  spanCheck.classList.toggle("form-error-hide", checkInput.checkValidity());
 
-submitBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    const spanCheck = document.querySelector('#checkbox-error');
-    const spanRadio = document.querySelector('#radio-error');
-    for (const input of inputs) {
-        input.classList.toggle('form-error-border', !input.checkValidity());
-        input.nextElementSibling.classList.toggle('form-error-hide', !input.classList.contains('form-error-border'));
-    }
-    for (const radioInput of radioInputs) {
-        spanRadio.classList.toggle('form-error-hide', radioInput.checkValidity());
-    }
-    spanCheck.classList.toggle('form-error-hide', checkInput.checkValidity());
-    
-    if (form.checkValidity()) {
-        dialog.showModal();
-    }
-} )
+  if (form.checkValidity()) {
+    dialog.showModal();
+  }
+});
